bcff0786bd9f1ccba265cb30b11a27eb7a38b4fb
Soheil <soheilhamidi@me.com>
2021-01-18 17:14:18 -0500

    Add favicon and update gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,18 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-plugin-sitemap`,
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/favicon.png`,
+      }
+    },
+    {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: "YOUR_GOOGLE_ANALYTICS_TRACKING_ID",
